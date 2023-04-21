bb9965c120bdf3a7d8525677235a61f8b01e699e
cristi josan <cristijosan132@gmail.com>
2023-04-21 12:31:09 +0300

    Fix movement and complete script for single player

--- Assets/Scripts/Character/Movement/CharacterMovement.cs
+++ Assets/Scripts/Character/Movement/CharacterMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using UnityEngine;
 
@@ -6,70 +7,80 @@ namespace Character.Movement
     public class CharacterMovement : MonoBehaviour
     {
         public float speed = 5.0f;
-    public float swipeThreshold = 50.0f;
-    public float holdTimeThreshold = 0.5f;
+        public float swipeThreshold = 50.0f;
+        public float holdTimeThreshold = 0.5f;
+        
 
-    private Rigidbody rigidBody;
-    private Vector2 swipeStartPosition;
-    private Vector2 swipeEndPosition;
-    private float holdStartTime;
-    private bool isHolding = false;
-    private bool isSwiping = false;
-    private Vector3 movementDirection = Vector3.zero;
+        private Vector2 _swipeStartPosition;
+        private Vector2 _swipeEndPosition;
+        private float _holdStartTime;
+        private bool _isHolding = false;
+        private bool _isSwiping = false;
+        private Vector3 _movementDirection = Vector3.zero;
 
-    void Start()
-    {
-        rigidBody = GetComponent<Rigidbody>();
-    }
 
-    void Update()
-    {
-        if (Input.touchCount > 0)
+        private void Update()
         {
-            Touch touch = Input.GetTouch(0);
+            if (Input.touchCount <= 0) return;
+            
+            var touch = Input.GetTouch(0);
 
-            if (touch.phase == TouchPhase.Began)
+            switch (touch.phase)
             {
-                swipeStartPosition = touch.position;
-                holdStartTime = Time.time;
-                isHolding = true;
-                isSwiping = false;
-            }
-            else if (touch.phase == TouchPhase.Moved && isHolding)
-            {
-                // Calculate the movement direction based on the swipe distance
-                Vector2 swipeDirection = touch.position - swipeStartPosition;
-                if (swipeDirection.magnitude > swipeThreshold)
+                case TouchPhase.Began:
+                    _swipeStartPosition = touch.position;
+                    _holdStartTime = Time.time;
+                    _isHolding = true;
+                    _isSwiping = false;
+                    break;
+                case TouchPhase.Moved when _isHolding:
                 {
-                    isSwiping = true;
-                    movementDirection = new Vector3(swipeDirection.x, 0.0f, swipeDirection.y).normalized;
-                }
-                else
-                {
-                    isSwiping = false;
-                    movementDirection = Vector3.zero;
-                }
-            }
-            else if (touch.phase == TouchPhase.Ended)
-            {
-                if (!isSwiping && Time.time - holdStartTime < holdTimeThreshold)
-                {
-                    // Perform some action when the user taps instead of swiping
-                    Debug.Log("Tap!");
-                }
+                    // Calculate the movement direction based on the swipe distance
+                    var swipeDirection = touch.position - _swipeStartPosition;
+                    if (swipeDirection.magnitude > swipeThreshold)
+                    {
+                        _isSwiping = true;
+                        _movementDirection = new Vector3(swipeDirection.x, 0.0f, swipeDirection.y).normalized;
+                    }
+                    else
+                    {
+                        _isSwiping = false;
+                        _movementDirection = Vector3.zero;
+                    }
 
-                // Reset the movement direction
-                movementDirection = Vector3.zero;
-                isHolding = false;
-                isSwiping = false;
+                    break;
+                }
+                case TouchPhase.Ended:
+                {
+                    if (!_isSwiping && Time.time - _holdStartTime < holdTimeThreshold)
+                    {
+                        // Perform some action when the user taps instead of swiping
+                        Debug.Log("Tap!");
+                    }
+
+                    // Reset the movement direction
+                    _movementDirection = Vector3.zero;
+                    _isHolding = false;
+                    _isSwiping = false;
+                    break;
+                }
+                case TouchPhase.Stationary:
+                    break;
+                case TouchPhase.Canceled:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
             }
         }
-    }
 
-    void FixedUpdate()
-    {
-        // Move the character in the movement direction
-        rigidBody.AddForce(movementDirection * speed, ForceMode.VelocityChange);
-    }
+        private void FixedUpdate()
+        {
+            if (!(_movementDirection.sqrMagnitude > 0.0f)) return;
+            
+            // Move the character in the movement direction
+            transform.position += _movementDirection * speed * Time.deltaTime;
+            var angle = Mathf.Atan2(_movementDirection.x, _movementDirection.z) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0, angle, 0);
+        }
     }
 }
