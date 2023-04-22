3bd9a13881e8bbb04d19847096b03ec22d9350eb
cristi josan <cristijosan132@gmail.com>
2023-04-22 13:22:12 +0300

    Add scanner, update character move(with rigidbody), Fix fov, Fix collision

--- Assets/Scripts/Character/Movement/CharacterMovement.cs
+++ Assets/Scripts/Character/Movement/CharacterMovement.cs
@@ -6,19 +6,26 @@ namespace Character.Movement
 {
     public class CharacterMovement : MonoBehaviour
     {
+        [Header("Character's setting")]
         public float speed = 5.0f;
+        [Header("Control's parameters")]
         public float swipeThreshold = 50.0f;
         public float holdTimeThreshold = 0.5f;
+        [Header("Character's components")]
+        public Animator characterAnimator;
+        public Rigidbody characterRigidbody;
         
-
+        // Animation key
+        private static readonly int Speed = Animator.StringToHash("Speed");
+        
+        // Private var
         private Vector2 _swipeStartPosition;
         private Vector2 _swipeEndPosition;
         private float _holdStartTime;
         private bool _isHolding = false;
         private bool _isSwiping = false;
         private Vector3 _movementDirection = Vector3.zero;
-
-
+        
         private void Update()
         {
             if (Input.touchCount <= 0) return;
@@ -60,6 +67,7 @@ namespace Character.Movement
 
                     // Reset the movement direction
                     _movementDirection = Vector3.zero;
+                    characterAnimator.SetFloat(Speed, 0);
                     _isHolding = false;
                     _isSwiping = false;
                     break;
@@ -77,10 +85,13 @@ namespace Character.Movement
         {
             if (!(_movementDirection.sqrMagnitude > 0.0f)) return;
             
-            // Move the character in the movement direction
-            transform.position += _movementDirection * speed * Time.deltaTime;
+            var moveSpeed = _movementDirection * speed * Time.deltaTime;
             var angle = Mathf.Atan2(_movementDirection.x, _movementDirection.z) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.Euler(0, angle, 0);
+            
+            characterAnimator.SetFloat(Speed, moveSpeed.magnitude);
+            
+            characterRigidbody.MovePosition(characterRigidbody.position + moveSpeed);
+            characterRigidbody.MoveRotation(Quaternion.Euler(0, angle, 0));
         }
     }
 }
